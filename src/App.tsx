f361a53928dc318a3bdcccfd2d4b43391ab31291
Roman Khassazhirov <roman.khassazhirov@gmail.com>
2024-09-25 14:20:49 +0300

    Add learn useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,25 @@
-import React, {MouseEvent} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { Button } from './components/Button';
 
 function App() {
 
-  // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-  //   console.log("Hello, I'm Roman");
-  // };
+  // let a = 1;
 
-  // const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-  //   console.log("Hello, I'm Ivan");
-  // };
+  let [a, setA] = useState<number>(1);
 
-  // const onClickHandler = (name: string) => {
-  //   console.log(`Hello my name is ${name}`);
-  // };
-
-  // const foo1 = () => {
-  //   console.log(100200);
-  // };
-  
-  // const foo2 = (num: number) => {
-  //   console.log(num);
-  // };
-
-  const Button1Foo = (subscriber: string, age: number) => {
-    console.log(subscriber, age);
+  const onClickHandler = () => {
+    setA(++a);
   };
 
-  const Button2Foo = (subscriber: string) => {
-    console.log(subscriber)
-  };
-
-  const StupidButton = () => {
-    console.log("I'm a stupid button");
+  const onClickResetHandler = () => {
+    setA(0);
   };
 
   return (
     <div className="App">
-      {/* <button onClick={() => {console.log('Hello')}}>MyYouTubeChannel-1</button> */}
-      {/* <button onClick={() => onClickHandler("Roman")}>MyYouTubeChannel-1</button> */}
-      {/* <button onClick={() => onClickHandler("Ivan")}>MyYouTubeChannel-2</button> */}
-
-      {/* <button onClick={foo1}>1</button> */}
-      {/* <button onClick={() => foo2(100200)}>2</button> */}
-
-
-      {/* <button>MyYouTubeChannel-1</button> */}
-      <Button name="MyYouTubeChannel-1" callBack={() => Button1Foo("Roman", 21)} />
-      {/* <button>MyYouTubeChannel-2</button> */}
-      <Button name="MyYouTubeChannel-2" callBack={() => Button2Foo("Ivan")} />
-
-      <Button name="I'm a stupid button" callBack={StupidButton}/>
-
+      <h1>{a}</h1>
+      <button onClick={onClickHandler}>number</button>
+      <button onClick={onClickResetHandler}>0</button>
     </div>
   );
 }
