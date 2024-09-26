40b17c08f3e59450b6a991020272629f00209788
Roman Khassazhirov <roman.khassazhirov@gmail.com>
2024-09-26 17:27:26 +0300

    Add method Filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
+import { Filter } from './Filter';
+
+export type FilterValuesType = 'all' | 'ruble' | 'dollar';
+
+export type MoneyType = {
+  banknote: string
+  nominal: number
+  number: string
+};
 
 function App() {
 
-  // let a = 1;
-
-  let [a, setA] = useState<number>(1);
-
-  const onClickHandler = () => {
-    setA(++a);
-  };
-
-  const onClickResetHandler = () => {
-    setA(0);
-  };
+  const [money, setMoney] = useState<Array<MoneyType>>([
+    {banknote: "dollar", nominal: 100, number: "a123456789"},
+    {banknote: "dollar", nominal: 50, number: "b123456789"},
+    {banknote: "ruble", nominal: 100, number: "c123456789"},
+    {banknote: "dollar", nominal: 100, number: "d123456789"},
+    {banknote: "dollar", nominal: 50, number: "e123456789"},
+    {banknote: "ruble", nominal: 100, number: "f123456789"},
+    {banknote: "dollar", nominal: 50, number: "j123456789"},
+    {banknote: "ruble", nominal: 50, number: "h123456789"}
+  ]);
 
   return (
-    <div className="App">
-      <h1>{a}</h1>
-      <button onClick={onClickHandler}>number</button>
-      <button onClick={onClickResetHandler}>0</button>
+    <div className='App'>
+      <Filter money={money} />
     </div>
   );
 }
